9cdedb7fcddbf63810d461505d17127a24587c7b
Mikhail Tolstoy <festivalmg75@gmail.com>
2017-04-14 06:09:41 +0500

    Create MainActivity layout and class

---
+++ app/src/main/java/com/festival/remindme/MainActivity.java
@@ -0,0 +1,17 @@
+package com.festival.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by user on 14.04.2017.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
